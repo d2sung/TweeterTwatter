05c355b8eeafb8d68489e078a85b3f05ca5859be
Daniel Sung <d2sung@ucsd.edu>
2017-02-27 01:06:30 -0800

    first commit

---
+++ TweeterTwatter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweeterTwatter
+//
+//  Created by Daniel Sung on 2/27/17.
+//  Copyright © 2017 Daniel Sung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
